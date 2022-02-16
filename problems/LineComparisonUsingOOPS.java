b57466bf32a2fe8bacd70e82875831683f0108d5
17harshu <harshalanandas17@gmail.com>
2022-02-16 18:12:03 +0530

    Welcome message

---
+++ problems/LineComparisonUsingOOPS.java
@@ -0,0 +1,7 @@
+package com.line.problems;
+
+public class LineComparisonUsingOOPS {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+}
