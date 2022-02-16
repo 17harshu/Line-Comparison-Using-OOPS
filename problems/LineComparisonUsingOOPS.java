5e92bafe9732778c4e2efadb54bbd198b8d60c0e
17harshu <harshalanandas17@gmail.com>
2022-02-16 18:30:52 +0530

    added: calculate length

--- problems/LineComparisonUsingOOPS.java
+++ problems/LineComparisonUsingOOPS.java
@@ -1,7 +1,26 @@
 package com.line.problems;
 
+import java.util.Scanner;
+
 public class LineComparisonUsingOOPS {
+	public static void calculateLength() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter first number- ");
+		int x1 = sc.nextInt();
+		System.out.print("Enter second number- ");
+		int y1 = sc.nextInt();
+		System.out.print("Enter third number- ");
+		int x2 = sc.nextInt();
+		System.out.print("Enter fourth number- ");
+		int y2 = sc.nextInt();
+
+		// Calculate distance between two points
+		double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("Distance between two points :" + distance);
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		calculateLength();
 	}
 }
